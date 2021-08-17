ac2e85063ea79d4927bf742c06c5131af50d4c9e
Unnar Bjarnarson <unnar@fuglar.com>
2021-08-17 16:15:36 +0000

    work in progress 2

--- apps/unemployment/pages/application/[step].tsx
+++ apps/unemployment/pages/application/[step].tsx
@@ -1,9 +1,6 @@
 import { UnemploymentStep } from './../../entities/enums/unemployment-step.enum'
 import { useRouter } from 'next/router'
 import React, { useState, useEffect } from 'react'
-
-
-
         
 const Index: React.FC = () => {
     const { query } = useRouter()
@@ -27,7 +24,7 @@ const Index: React.FC = () => {
     }
   
     const changesMade = () => {
-      getApplication(clientId as string)
+      getApplication()
     }
   
     const handleNext = () => {
@@ -54,172 +51,11 @@ const Index: React.FC = () => {
     switch (step) {
       case UnemploymentStep.PersonalInformation:
         return (
-
-          <ContentWrapper>
-            <ClientStepNav handleStepChange={handleStepChange} activeStep={step}>
-              <ClientCreateForm
-                handleCancel={handleCancel}
-                client={client as ClientDTO}
-                onNextButtonClick={handleSaved}
-              />
-            </ClientStepNav>
-          </ContentWrapper>
-          <
+            <div className="test"></div>
         )
-      case ClientStep.ClientRedirectUri: {
-        return (
-          <ContentWrapper>
-            <ClientStepNav handleStepChange={handleStepChange} activeStep={step}>
-              <ClientRedirectUriForm
-                clientId={client.clientId}
-                defaultUrl={client.clientUri}
-                uris={client.redirectUris?.map((r) => r.redirectUri)}
-                handleNext={handleNext}
-                handleBack={handleBack}
-                handleChanges={changesMade}
-              />
-            </ClientStepNav>
-          </ContentWrapper>
-        )
-      }
-      case ClientStep.ClientIdpRestrictions: {
-        return (
-          <ContentWrapper>
-            <ClientStepNav handleStepChange={handleStepChange} activeStep={step}>
-              <ClientIdpRestrictionsForm
-                clientId={client.clientId}
-                restrictions={client.identityProviderRestrictions?.map(
-                  (r) => r.name,
-                )}
-                handleNext={handleNext}
-                handleBack={handleBack}
-                handleChanges={changesMade}
-              />
-            </ClientStepNav>
-          </ContentWrapper>
-        )
-      }
-      case ClientStep.ClientPostLogoutRedirectUri: {
-        return (
-          <ContentWrapper>
-            <ClientStepNav handleStepChange={handleStepChange} activeStep={step}>
-              <ClientPostLogoutRedirectUriForm
-                clientId={client.clientId}
-                defaultUrl={client.clientUri}
-                uris={client.postLogoutRedirectUris?.map((p) => p.redirectUri)}
-                handleNext={handleNext}
-                handleBack={handleBack}
-                handleChanges={changesMade}
-              />
-            </ClientStepNav>
-          </ContentWrapper>
-        )
-      }
-      case ClientStep.ClientAllowedCorsOrigin: {
-        return (
-          <ContentWrapper>
-            <ClientStepNav handleStepChange={handleStepChange} activeStep={step}>
-              <ClientAllowedCorsOriginsForm
-                clientId={client.clientId}
-                defaultOrigin={client.clientUri}
-                origins={client.allowedCorsOrigins?.map((a) => a.origin)}
-                handleNext={handleNext}
-                handleBack={handleBack}
-                handleChanges={changesMade}
-              />
-            </ClientStepNav>
-          </ContentWrapper>
-        )
-      }
-      case ClientStep.ClientGrantTypes: {
-        return (
-          <ContentWrapper>
-            <ClientStepNav handleStepChange={handleStepChange} activeStep={step}>
-              <ClientGrantTypesForm
-                clientId={client.clientId}
-                grantTypes={client.allowedGrantTypes?.map((a) => a.grantType)}
-                handleBack={handleBack}
-                handleChanges={changesMade}
-                handleNext={handleNext}
-              />
-            </ClientStepNav>
-          </ContentWrapper>
-        )
-      }
-      case ClientStep.ClientAllowedScopes: {
-        return (
-          <ContentWrapper>
-            <ClientStepNav handleStepChange={handleStepChange} activeStep={step}>
-              <ClientAllowedScopesForm
-                clientId={client.clientId}
-                scopes={client.allowedScopes?.map((s) => s.scopeName)}
-                handleChanges={changesMade}
-                handleNext={handleNext}
-                handleBack={handleBack}
-              />
-            </ClientStepNav>
-          </ContentWrapper>
-        )
-      }
-      case ClientStep.ClientClaims: {
-        return (
-          <ContentWrapper>
-            <ClientStepNav handleStepChange={handleStepChange} activeStep={step}>
-              <ClientClaimForm
-                clientId={client.clientId}
-                claims={client.claims}
-                handleNext={handleNext}
-                handleBack={handleBack}
-                handleChanges={changesMade}
-              ></ClientClaimForm>
-            </ClientStepNav>
-          </ContentWrapper>
-        )
-      }
-      case ClientStep.ClientSecret: {
-        return (
-          <ContentWrapper>
-            <ClientStepNav handleStepChange={handleStepChange} activeStep={step}>
-              <ClientSecretForm
-                secrets={client.clientSecrets}
-                clientId={client.clientId}
-                clientType={client.clientType}
-                handleBack={handleBack}
-                handleNext={handleNext}
-                handleChanges={changesMade}
-              />
-            </ClientStepNav>
-          </ContentWrapper>
-        )
-      }
       default: {
         return (
-          <ContentWrapper>
-            <ClientStepNav handleStepChange={handleStepChange} activeStep={step}>
-              <StepEnd
-                buttonText={
-                  LocalizationUtils.getPage('client.[client]').endStep?.buttonText
-                }
-                title={
-                  LocalizationUtils.getPage('client.[client]').endStep?.title
-                }
-                handleButtonFinishedClick={() => setStep(1)}
-              >
-                <p>
-                  {
-                    LocalizationUtils.getPage('client.[client]').endStep
-                      ?.infoTitle
-                  }
-                </p>
-                <p>
-                  {
-                    LocalizationUtils.getPage('client.[client]').endStep
-                      ?.infoDescription
-                  }
-                </p>
-              </StepEnd>
-            </ClientStepNav>
-          </ContentWrapper>
+          <div className="test2"></div>
         )
       }
     }
