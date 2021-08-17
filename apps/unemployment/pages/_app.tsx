0603e22f3cdad9f6bf6ff9b2bc0993b59eb7b310
Unnar Bjarnarson <unnar@fuglar.com>
2021-08-17 16:02:05 +0000

    ekki rétt

--- apps/unemployment/pages/_app.tsx
+++ apps/unemployment/pages/_app.tsx
@@ -5,6 +5,7 @@ import './styles.css'
 import Header from '../components/layout/header'
 import ContentWrapper from '../components/layout/content-wrapper'
 import UserProvider from '../components/util/UserProvider'
+import { GridRow, GridColumn, GridContainer } from '@island.is/island-ui/core'
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -12,10 +13,20 @@ function CustomApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Atvinnuleysisbætur</title>
       </Head>
-      <ContentWrapper>
-      <Header></Header>
-        <Component {...pageProps} />
-      </ContentWrapper>      
+      <GridContainer>
+        <GridRow>
+          <GridColumn span={['12/12', '12/12', '12/12', '12/12', '12/12']}>
+            <Header></Header>
+          </GridColumn>
+        </GridRow>
+        <GridRow>
+          <GridColumn>
+            <ContentWrapper>
+              <Component {...pageProps} />
+            </ContentWrapper>      
+          </GridColumn>
+        </GridRow>
+      </GridContainer>
     </UserProvider>
   )
 }
