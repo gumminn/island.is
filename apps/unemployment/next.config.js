cc544cc3f1c7a0629ced6bce2147d792f7e3bec2
Guðmundur Sæmundsen <gudmundur@fuglar.com>
2021-08-18 02:27:48 +0000

    Breytingarnar teknar til baka

--- apps/unemployment/next.config.js
+++ apps/unemployment/next.config.js
@@ -2,9 +2,3 @@ const withTreat = require('next-treat')()
 const withSourceMaps = require('@zeit/next-source-maps')
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 module.exports = withSourceMaps(withTreat({}))
-module.exports = withNx({
-  target: 'experimental-serverless-trace', // <-
-  trailingSlash: true,
-  reactStrictMode: true,
-  env: ...
-})
