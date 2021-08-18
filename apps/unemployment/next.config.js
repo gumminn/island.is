73ed110310365972e2aae41ebb5046454e5b04c1
Guðmundur Sæmundsen <gudmundur@fuglar.com>
2021-08-18 01:58:30 +0000

    Reyna að laga "No Serverless Pages Built" fyrir Vercel, https://github.com/vercel/vercel/blob/main/errors/now-next-no-serverless-pages-built.md

--- apps/unemployment/next.config.js
+++ apps/unemployment/next.config.js
@@ -2,3 +2,7 @@ const withTreat = require('next-treat')()
 const withSourceMaps = require('@zeit/next-source-maps')
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 module.exports = withSourceMaps(withTreat({}))
+module.exports = {
+  target: 'serverless',
+  // Other options
+};
